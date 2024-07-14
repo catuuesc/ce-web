86d680210dff5c406c22bc9332e51ebeae51e7e2
Catalina Escarate <cimientoestudio@gmail.com>
2024-07-13 23:52:40 -0400

    finalizacion de edicion de pagina

--- paginas/form.php
+++ paginas/form.php
@@ -1,24 +1,34 @@
 <?php
-/*configurar cada uno de los imput con el metodo de almacenar info POST*/ 
-$nombre = $_POST ['nombre']
-$correo = $_POST ['correo']
-$numeros = $_POST ['numero']
-$comentarios = $_POST ['comentarios']
+// Configurar cada uno de los inputs con el método POST
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$numeros = $_POST['numero'];
+$comentarios = $_POST['comentarios'];
 
-/*para hacer que llegue de cierta manera al correo que asignemos*/
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Desde la dirección de correo: " .$correo . ",\r\n";
-$mensaje .= "Su número de contacto es: " .$numeros . ",\r\n";
-$mensaje .= "Mensaje: " .$comentarios . ",\r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+// Validar que todos los campos estén completos
+if (!empty($nombre) && !empty($correo) && !empty($numeros) && !empty($comentarios)) {
+    // Crear el header
+    $header = "Content-Type: text/plain";
 
-/*Datos de donde tiene que llegar la información*/
-$para = 'cimientoestudio@gmail.com';
-$asunto = 'Contacto desde formulario Cimiento Estudio';
+    // Crear el mensaje
+    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+    $mensaje .= "Desde la dirección de correo: " . $correo . ",\r\n";
+    $mensaje .= "Su número de contacto es: " . $numeros . ",\r\n";
+    $mensaje .= "Mensaje: " . $comentarios . ",\r\n";
+    $mensaje .= "Enviado el " . date("d/m/Y", time());
 
-/*Redirección al enviar el formulario*/
-header('Location:gracias.html');
+    // Datos de donde tiene que llegar la información
+    $para = "cimientoestudio@gmail.com";
+    $asunto = "Contacto desde formulario Cimiento Estudio";
 
-/*funcion mail*/
-mail($para, $asunto, utf8_decode($mensaje), $header);
+    // Enviar el correo
+    if (mail($para, $asunto, utf8_decode($mensaje), $header)) {
+        // Redirección al enviar el formulario
+        header("Location: gracias.html");
+    } else {
+        echo "Hubo un error al enviar el mensaje. Por favor, intenta de nuevo.";
+    }
+} else {
+    echo "Por favor, completa todos los campos.";
+}
 ?>
